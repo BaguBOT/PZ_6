38e2547388fedd4c4a0651b9ee0c5027b8916721
Александр Индинок <aleksandar_34@mail.ru>
2023-02-17 15:34:04 +0700

    Добавьте файлы проекта.

---
+++ PZ_6/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+var ATEST = new List<Student>
+{
+  new Student("alex",4),
+    new Student("bob",5),
+      new Student("duma",3),
+        new Student("Boba",2),
+};
+PrintGoodStudents();
+void PrintGoodStudents()
+{
+    var goodStudens = ATEST.Where(s => s.Grade >= 4) 
+            .ToList();
+    goodStudens.ForEach(s => Console.WriteLine(s.Name)); 
+}
+
+
+class Student
+{
+   
+ public string Name { get; private set; }
+    public int Grade { get; private set; }
+    public Student(string name, int grade)
+    {
+       Name = name;
+        Grade = grade;
+    }
+}
