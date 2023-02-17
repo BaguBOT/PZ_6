a5aebce0f04a301623b614cdf44b2f9b38c576f0
Александр Индинок <aleksandar_34@mail.ru>
2023-02-17 15:43:25 +0700

    Так

--- PZ_6/Program.cs
+++ PZ_6/Program.cs
@@ -8,14 +8,28 @@ var ATEST = new List<Student>
       new Student("duma",3),
         new Student("Boba",2),
 };
+
 PrintGoodStudents();
+Console.WriteLine("///");
+PrintbadStudents();
 void PrintGoodStudents()
 {
     var goodStudens = ATEST.Where(s => s.Grade >= 4) 
             .ToList();
     goodStudens.ForEach(s => Console.WriteLine(s.Name)); 
 }
-
+void PrintbadStudents()
+{
+    var badStudens = ATEST.Where(s => s.Grade < 4)
+            .ToList();
+    badStudens.ForEach(s => Console.WriteLine(s.Name));
+  
+    var NOAtest = new List<Student> { }
+;
+    foreach (var ATEST in NOAtest)
+        NOAtest.Add(ATEST);
+    Console.WriteLine(NOAtest);
+}
 
 class Student
 {
